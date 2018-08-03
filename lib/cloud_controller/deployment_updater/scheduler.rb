26801a48ef601671698639df69702aa5b9a1f06e
Chris Selzo <cselzo@pivotal.io>
2018-08-03 15:05:08 -0700

    Backfill tests for deployment update performance
    Signed-off-by: Eli Wrenn <ewrenn@pivotal.io>

--- lib/cloud_controller/deployment_updater/scheduler.rb
+++ lib/cloud_controller/deployment_updater/scheduler.rb
@@ -5,31 +5,40 @@ require 'locket/lock_runner'
 module VCAP::CloudController
   module DeploymentUpdater
     class Scheduler
-      def self.start
-        config = CloudController::DependencyLocator.instance.config
-        update_frequency = config.get(:deployment_updater, :update_frequency_in_seconds)
-        logger = Steno.logger('cc.deployment_updater.scheduler')
+      class << self
+        def start
+          config = CloudController::DependencyLocator.instance.config
+          lock_runner = Locket::LockRunner.new(
+            key: config.get(:deployment_updater, :lock_key),
+            owner: config.get(:deployment_updater, :lock_owner),
+            host: config.get(:locket, :host),
+            port: config.get(:locket, :port),
+            client_ca_path: config.get(:locket, :ca_file),
+            client_key_path: config.get(:locket, :key_file),
+            client_cert_path: config.get(:locket, :cert_file),
+          )
 
-        lock_runner = Locket::LockRunner.new(
-          key: config.get(:deployment_updater, :lock_key),
-          owner: config.get(:deployment_updater, :lock_owner),
-          host: config.get(:locket, :host),
-          port: config.get(:locket, :port),
-          client_ca_path: config.get(:locket, :ca_file),
-          client_key_path: config.get(:locket, :key_file),
-          client_cert_path: config.get(:locket, :cert_file),
-        )
+          lock_worker = Locket::LockWorker.new(lock_runner)
 
-        lock_worker = Locket::LockWorker.new(lock_runner)
+          lock_worker.acquire_lock_and do
+            update(config.get(:deployment_updater, :update_frequency_in_seconds))
+          end
+        end
 
-        lock_worker.acquire_lock_and do
-          t1 = Time.now
+        private
+
+        def update(update_frequency)
+          logger = Steno.logger('cc.deployment_updater.scheduler')
+
+          update_start_time = Time.now
           Updater.update
-          update_duration = Time.now - t1
-          logger.info("Update loop took #{Time.now - t1}s")
-          if update_duration < update_frequency
-            logger.info("Sleeping #{update_frequency - update_duration}s")
-            sleep(update_frequency - update_duration)
+          update_duration = Time.now - update_start_time
+          logger.info("Update loop took #{update_duration}s")
+
+          sleep_duration = update_frequency - update_duration
+          if sleep_duration > 0
+            logger.info("Sleeping #{sleep_duration}s")
+            sleep(sleep_duration)
           else
             logger.info('Not Sleeping')
           end
--- lib/cloud_controller/deployment_updater/updater.rb
+++ lib/cloud_controller/deployment_updater/updater.rb
@@ -3,88 +3,94 @@ require 'actions/process_restart'
 module VCAP::CloudController
   module DeploymentUpdater
     class Updater
-      def self.update
-        logger = Steno.logger('cc.deployment_updater.update')
-        logger.info('run-deployment-update')
+      class << self
 
-        deployments = DeploymentModel.where(state: DeploymentModel::DEPLOYING_STATE)
-        workpool = WorkPool.new(50)
+        def update
+          logger = Steno.logger('cc.deployment_updater.update')
+          logger.info('run-deployment-update')
 
-        deployments.each do |deployment|
-          workpool.submit(deployment, logger) do | d, l |
-            begin
-              scale_deployment(d, l)
-            rescue => e
-              error_name = e.is_a?(CloudController::Errors::ApiError) ? e.name : e.class.name
-              logger.error(
-                'error-scaling-deployment',
-                deployment_guid: d.guid,
-                error: error_name,
-                error_message: e.message,
-                backtrace: e.backtrace.join("\n")
-              )
+          deployments = DeploymentModel.where(state: DeploymentModel::DEPLOYING_STATE)
+          begin
+            workpool = WorkPool.new(50)
+
+            deployments.each do |deployment|
+              workpool.submit(deployment, logger) do | d, l |
+                begin
+                  scale_deployment(d, l)
+                rescue => e
+                  error_name = e.is_a?(CloudController::Errors::ApiError) ? e.name : e.class.name
+                  logger.error(
+                    'error-scaling-deployment',
+                    deployment_guid: d.guid,
+                    error: error_name,
+                    error_message: e.message,
+                    backtrace: e.backtrace.join("\n")
+                  )
+                end
+              end
+            end
+
+          ensure
+            workpool.drain
+          end
+        end
+
+        private
+
+        def scale_deployment(deployment, logger)
+          deployment.db.transaction do
+            deployment.lock!
+
+            app = deployment.app
+            original_web_process = app.web_process
+            deploying_web_process = deployment.deploying_web_process
+
+            app.lock!
+            original_web_process.lock!
+            deploying_web_process.lock!
+
+            return unless ready_to_scale?(deployment, logger)
+
+            case original_web_process.instances
+            when 0 # deploying web process is fully scaled
+              promote_deploying_web_process(deploying_web_process, original_web_process)
+
+              restart_non_web_processes(app)
+              deployment.update(state: DeploymentModel::DEPLOYED_STATE)
+            when 1 # do not increment deploying web process because upon deploy, an initial deploying web process was created
+              original_web_process.update(instances: original_web_process.instances - 1)
+            else
+              original_web_process.update(instances: original_web_process.instances - 1)
+              deploying_web_process.update(instances: deploying_web_process.instances + 1)
             end
           end
+
+          logger.info("ran-deployment-update-for-#{deployment.guid}")
         end
 
-        workpool.drain
-      end
+        def ready_to_scale?(deployment, logger)
+          instances = instance_reporters.all_instances_for_app(deployment.deploying_web_process)
+          instances.all? { |_, val| val[:state] == VCAP::CloudController::Diego::LRP_RUNNING }
+        rescue CloudController::Errors::ApiError # the instances_reporter re-raises InstancesUnavailable as ApiError
+          logger.info("skipping-deployment-update-for-#{deployment.guid}")
+          return false
+        end
 
-      private_class_method
+        def instance_reporters
+          CloudController::DependencyLocator.instance.instances_reporters
+        end
 
-      def self.scale_deployment(deployment, logger)
-        deployment.db.transaction do
-          deployment.lock!
+        def promote_deploying_web_process(deploying_web_process, original_web_process)
+          deploying_web_process.update(type: ProcessTypes::WEB)
+          original_web_process.delete
+        end
 
-          app = deployment.app
-          original_web_process = app.web_process
-          deploying_web_process = deployment.deploying_web_process
-
-          app.lock!
-          original_web_process.lock!
-          deploying_web_process.lock!
-
-          return unless ready_to_scale?(deployment, logger)
-
-          case original_web_process.instances
-          when 0 # deploying web process is fully scaled
-            promote_deploying_web_process(deploying_web_process, original_web_process)
-
-            restart_non_web_processes(app)
-            deployment.update(state: DeploymentModel::DEPLOYED_STATE)
-          when 1 # do not increment deploying web process because upon deploy, an initial deploying web process was created
-            original_web_process.update(instances: original_web_process.instances - 1)
-          else
-            original_web_process.update(instances: original_web_process.instances - 1)
-            deploying_web_process.update(instances: deploying_web_process.instances + 1)
+        def restart_non_web_processes(app)
+          app.processes.reject(&:web?).each do |process|
+            VCAP::CloudController::ProcessRestart.restart(process: process, config: Config.config, stop_in_runtime: true)
           end
         end
-
-        logger.info("ran-deployment-update-for-#{deployment.guid}")
-      end
-
-      def self.ready_to_scale?(deployment, logger)
-        instances = instance_reporters.all_instances_for_app(deployment.deploying_web_process)
-        instances.all? { |_, val| val[:state] == VCAP::CloudController::Diego::LRP_RUNNING }
-      rescue CloudController::Errors::ApiError # the instances_reporter re-raises InstancesUnavailable as ApiError
-        logger.info("skipping-deployment-update-for-#{deployment.guid}")
-        return false
-      end
-
-      def self.instance_reporters
-        CloudController::DependencyLocator.instance.instances_reporters
-      end
-
-      def self.promote_deploying_web_process(deploying_web_process, original_web_process)
-        deploying_web_process.update(type: ProcessTypes::WEB)
-        original_web_process.delete
-      end
-
-      def self.restart_non_web_processes(app)
-        app.processes.reject(&:web?).each do |process|
-          VCAP::CloudController::ProcessRestart.restart(process: process, config: Config.config, stop_in_runtime: true)
         end
       end
-    end
   end
 end
